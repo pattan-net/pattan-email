1a2cdecb99a15a1636d88f839d0466fe58ef85a1
Markus <mweltin@pattan.net>
2024-04-25 10:54:47 -0700

    s1297 further decouple from django

--- pattan_email/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CommunicationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = "pattan_email"
-    label = "pattan_email"

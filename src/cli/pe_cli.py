f7738ebaa1af698369a3553f68e31de5efaba282
mweltin <mweltin@pattan.net>
2025-04-03 09:53:45 -1000

    s1516 cli creates a SendGridAPIClient object

--- src/cli/pe_cli.py
+++ src/cli/pe_cli.py
@@ -1,12 +1,17 @@
 # cli.py
+import os
 import click
+from sendgrid import SendGridAPIClient
+from dotenv import load_dotenv
 
+load_dotenv('../../.env')
 
 @click.command()
 @click.option('--name', default='World', help='Name to greet')
 def pe_cli(name):
     """Simple program that greets NAME."""
     click.echo(f'Hello, {name}!')
+    sg = SendGridAPIClient(api_key=os.getenv('SENDGRID_API_KEY'))
 
 
 if __name__ == '__main__':
